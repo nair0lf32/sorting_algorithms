c9bb9922eff677225e80c11e40337fb3c670503a
nair0lf32 <florianedem@gmail.com>
2022-07-22 17:39:05 +0100

    review tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,26 +7,24 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0;
-	unsigned int temp;
-	int noswap = 0;
-
-	if (array == NULL || size < 2)
-		return;
-
-	while (noswap == 0)
-	{
-		noswap = 1;
-		for (i = 0; i < size - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				noswap = 0;
-				print_array(array, size);
-			}
-		}
-	}
+size_t i = 0;
+unsigned int temp;
+int noswap = 0;
+if (array == NULL || size < 2)
+	return;
+while (noswap == 0)
+{
+noswap = 1;
+for (i = 0; i < size - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+temp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = temp;
+noswap = 0;
+print_array(array, size);
+}
+}
+}
 }
